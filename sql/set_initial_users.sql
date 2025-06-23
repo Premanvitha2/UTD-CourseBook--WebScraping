3d681adde190bbc206f5b67aa16e759c387276ad
Premanvitha2 <saipremanvitha.kadiyala2000@gmail.com>
2025-06-23 10:47:42 -0500

    Delete sql directory

--- sql/set_initial_users.sql
+++
@@ -1,8 +0,0 @@
--- SQL Insert Statements for Initial Users
-
-INSERT INTO user (userid, password, user_type, user_status, first_name, last_name, email, created_at)
-VALUES ('admin1', 'HASHED_PASSWORD_HERE', 'admin', 'active', 'Admin', 'Name1', 'admin1@utdallas.edu', '2025-03-10 15:30:00');
-
-INSERT INTO user (userid, password, user_type, user_status, first_name, last_name, email, created_at)
-VALUES ('user2', 'HASHED_PASSWORD_HERE', 'user', 'active', 'User', 'Name2', 'user2@utdallas.edu', '2025-03-11 09:10:01');
-
